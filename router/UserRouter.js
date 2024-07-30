58ab26a0b58eae59aebb2ff998e7c1f666c00052
Yuseydi <pamelacamisan26@gmail.com>
2024-07-30 08:47:39 -0500

    Api con NODE

---
+++ router/UserRouter.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { login,updateUsersPassword, updateUsersEmail, getUsers,createUsers,updateUsers,deleteUsers,getOneUser, searchUsersByName} from '../controller/UserController.js';
+import  {verifyToken}  from '../middleware/auth.js';
+const rotuer = express.Router();
+rotuer.get('/user', getUsers);
+rotuer.get('/user/:id',verifyToken, getOneUser);
+rotuer.post('/register', createUsers);
+rotuer.put('/user/:id',verifyToken, updateUsers);
+rotuer.delete('/user/:id', verifyToken, deleteUsers);
+rotuer.post('/login', login);
+rotuer.put('/user/email/:id',verifyToken, updateUsersEmail);
+rotuer.put('/user/password/:id',verifyToken, updateUsersPassword);
+rotuer.get('/user/search/:name',     searchUsersByName);
+export const RouterUsuer = rotuer;
